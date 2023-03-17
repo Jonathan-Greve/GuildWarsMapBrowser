407c3cc05e5e373a7d1143fcdb309124a1951a1c
Jonathan-Greve <jonagrev@gmail.com>
2023-03-17 18:15:58 +0800

    Refactored the box generator code to be more readable. And correct!

--- Box.h
+++ Box.h
@@ -16,47 +16,75 @@ private:
         float half_height = size.y * 0.5f;
         float half_depth = size.z * 0.5f;
 
+        // Define the 8 corner points of the box.
+        DirectX::XMFLOAT3 topLeftFront(-half_width, half_height, -half_depth);
+        DirectX::XMFLOAT3 topLeftBack(-half_width, half_height, half_depth);
+        DirectX::XMFLOAT3 topRightFront(half_width, half_height, -half_depth);
+        DirectX::XMFLOAT3 topRightBack(half_width, half_height, half_depth);
+        DirectX::XMFLOAT3 bottomLeftFront(-half_width, -half_height, -half_depth);
+        DirectX::XMFLOAT3 bottomLeftBack(-half_width, -half_height, half_depth);
+        DirectX::XMFLOAT3 bottomRightFront(half_width, -half_height, -half_depth);
+        DirectX::XMFLOAT3 bottomRightBack(half_width, -half_height, half_depth);
+
+        // Define the normals for each face of the box.
+        DirectX::XMFLOAT3 frontNormal(0.0f, 0.0f, -1.0f);
+        DirectX::XMFLOAT3 backNormal(0.0f, 0.0f, 1.0f);
+        DirectX::XMFLOAT3 leftNormal(-1.0f, 0.0f, 0.0f);
+        DirectX::XMFLOAT3 rightNormal(1.0f, 0.0f, 0.0f);
+        DirectX::XMFLOAT3 topNormal(0.0f, 1.0f, 0.0f);
+        DirectX::XMFLOAT3 bottomNormal(0.0f, -1.0f, 0.0f);
+
+        // Define the texture coordinates for each vertex.
+        DirectX::XMFLOAT2 tex00(0.0f, 0.0f);
+        DirectX::XMFLOAT2 tex01(0.0f, 1.0f);
+        DirectX::XMFLOAT2 tex10(1.0f, 0.0f);
+        DirectX::XMFLOAT2 tex11(1.0f, 1.0f);
+
         std::vector<Vertex> vertices = {
           // Front face
-          {{-half_width, -half_height, -half_depth}, {0, 0, -1}, {0, 1}},
-          {{half_width, -half_height, -half_depth}, {0, 0, -1}, {1, 1}},
-          {{half_width, half_height, -half_depth}, {0, 0, -1}, {1, 0}},
-          {{-half_width, half_height, -half_depth}, {0, 0, -1}, {0, 0}},
+          {topLeftFront, frontNormal, tex11},
+          {bottomLeftFront, frontNormal, tex01},
+          {bottomRightFront, frontNormal, tex00},
+          {topRightFront, frontNormal, tex10},
           // Back face
-          {{-half_width, -half_height, half_depth}, {0, 0, 1}, {1, 1}},
-          {{half_width, -half_height, half_depth}, {0, 0, 1}, {0, 1}},
-          {{half_width, half_height, half_depth}, {0, 0, 1}, {0, 0}},
-          {{-half_width, half_height, half_depth}, {0, 0, 1}, {1, 0}},
-          // Top face
-          {{-half_width, half_height, -half_depth}, {0, 1, 0}, {0, 1}},
-          {{half_width, half_height, -half_depth}, {0, 1, 0}, {1, 1}},
-          {{half_width, half_height, half_depth}, {0, 1, 0}, {1, 0}},
-          {{-half_width, half_height, half_depth}, {0, 1, 0}, {0, 0}},
-          // Bottom face
-          {{-half_width, -half_height, -half_depth}, {0, -1, 0}, {1, 1}},
-          {{half_width, -half_height, -half_depth}, {0, -1, 0}, {0, 1}},
-          {{half_width, -half_height, half_depth}, {0, -1, 0}, {0, 0}},
-          {{-half_width, -half_height, half_depth}, {0, -1, 0}, {1, 0}},
+          {topRightBack, backNormal, tex11},
+          {bottomRightBack, backNormal, tex01},
+          {bottomLeftBack, backNormal, tex00},
+          {topLeftBack, backNormal, tex10},
           // Left face
-          {{-half_width, -half_height, -half_depth}, {-1, 0, 0}, {1, 1}},
-          {{-half_width, half_height, -half_depth}, {-1, 0, 0}, {1, 0}},
-          {{-half_width, half_height, half_depth}, {-1, 0, 0}, {0, 0}},
-          {{-half_width, -half_height, half_depth}, {-1, 0, 0}, {0, 1}},
+          {topLeftBack, leftNormal, tex11},
+          {bottomLeftBack, leftNormal, tex01},
+          {bottomLeftFront, leftNormal, tex00},
+          {topLeftFront, leftNormal, tex10},
           // Right face
-          {{half_width, -half_height, -half_depth}, {1, 0, 0}, {0, 1}},
-          {{half_width, half_height, -half_depth}, {1, 0, 0}, {0, 0}},
-          {{half_width, half_height, half_depth}, {1, 0, 0}, {1, 0}},
-          {{half_width, -half_height, half_depth}, {1, 0, 0}, {1, 1}},
+          {topRightFront, rightNormal, tex11},
+          {bottomRightFront, rightNormal, tex01},
+          {bottomRightBack, rightNormal, tex00},
+          {topRightBack, rightNormal, tex10},
+          // Top face
+          {topLeftBack, topNormal, tex11},
+          {topLeftFront, topNormal, tex01},
+          {topRightFront, topNormal, tex00},
+          {topRightBack, topNormal, tex10},
+          // Bottom face
+          {bottomLeftFront, bottomNormal, tex11},
+          {bottomLeftBack, bottomNormal, tex01},
+          {bottomRightBack, bottomNormal, tex00},
+          {bottomRightFront, bottomNormal, tex10},
         };
 
-        std::vector<uint32_t> indices = {
-          0,  2,  1,  0,  3,  2, // Front face
-          4,  6,  5,  4,  7,  6, // Back face
-          8,  10, 9,  8,  11, 10, // Top face
-          12, 14, 13, 12, 15, 14, // Bottom face
-          16, 18, 17, 16, 19, 18, // Left face
-          20, 22, 21, 20, 23, 22, // Right face
-        };
+        std::vector<uint32_t> indices = {// Front face
+                                         1, 0, 3, 1, 3, 2,
+                                         // Back face
+                                         5, 4, 7, 5, 7, 6,
+                                         // Left face
+                                         9, 8, 11, 9, 11, 10,
+                                         // Right face
+                                         13, 12, 15, 13, 15, 14,
+                                         // Top face
+                                         17, 16, 19, 17, 19, 18,
+                                         // Bottom face
+                                         21, 20, 23, 21, 23, 22};
 
         return Mesh(vertices, indices);
     }
