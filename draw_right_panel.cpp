f3fbd76d0cddee9e73dad807dae83db23bc047d2
Jonathan-Greve <jonagrev@gmail.com>
2023-03-22 07:46:11 +0800

    Added 'Camera and movement' to the right panel. This allow the user to change camera settings, movement setttings and change between orthographic and perspective projection.

--- draw_right_panel.cpp
+++ draw_right_panel.cpp
@@ -15,32 +15,35 @@ void draw_right_panel(MapRenderer* map_renderer)
     ImGui::PushStyleVar(
       ImGuiStyleVar_WindowPadding,
       ImVec2(GuiGlobalConstants::panel_padding, GuiGlobalConstants::panel_padding)); // add padding
-    ImGui::Begin("Render settings", NULL, window_flags);
-
-    static float water_level = 0.0f;
-
-    auto terrain = map_renderer->GetTerrain();
-    if (terrain)
+    float window_height = 0;
+    if (ImGui::Begin("Render settings", NULL, window_flags))
     {
-        float min_level = terrain->m_bounds.map_min_y;
-        float max_level = terrain->m_bounds.map_max_y;
 
-        // Create the slider for changing the water level with text input enabled
-        if (ImGui::SliderFloat("Water level", &water_level, min_level, max_level, "%.2f", 0))
+        static float water_level = 0.0f;
+
+        auto terrain = map_renderer->GetTerrain();
+        if (terrain)
         {
-            water_level = ImClamp(water_level, min_level, max_level);
-            map_renderer->UpdateTerrainWaterLevel(water_level);
+            float min_level = terrain->m_bounds.map_min_y;
+            float max_level = terrain->m_bounds.map_max_y;
+
+            // Create the slider for changing the water level with text input enabled
+            if (ImGui::SliderFloat("Water level", &water_level, min_level, max_level, "%.2f", 0))
+            {
+                water_level = ImClamp(water_level, min_level, max_level);
+                map_renderer->UpdateTerrainWaterLevel(water_level);
+            }
+
+            int rasterizerState = static_cast<int>(map_renderer->GetCurrentRasterizerState());
+            if (ImGui::Combo("Rasterizer State", &rasterizerState,
+                             "Solid\0Solid no cull\0Wireframe\0Wireframe no cull\0"))
+            {
+                map_renderer->SwitchRasterizerState(static_cast<RasterizerStateType>(rasterizerState));
+            }
         }
 
-        int rasterizerState = static_cast<int>(map_renderer->GetCurrentRasterizerState());
-        if (ImGui::Combo("Rasterizer State", &rasterizerState,
-                         "Solid\0Solid_NoCull\0Wireframe\0Wireframe_NoCull\0"))
-        {
-            map_renderer->SwitchRasterizerState(static_cast<RasterizerStateType>(rasterizerState));
-        }
+        window_height = ImGui::GetWindowSize().y;
     }
-
-    float window_height = ImGui::GetWindowSize().y;
     ImGui::End();
 
     // Set up the second right panel
@@ -49,10 +52,107 @@ void draw_right_panel(MapRenderer* map_renderer)
                GuiGlobalConstants::panel_padding,
              GuiGlobalConstants::panel_padding + window_height + GuiGlobalConstants::panel_padding));
     ImGui::SetNextWindowSize(ImVec2(GuiGlobalConstants::right_panel_width, 0));
-    ImGui::Begin("Second panel", NULL, window_flags);
+    if (ImGui::Begin("Camera and movement", NULL, window_flags))
+    {
+        bool camera_projection_settings_changed = false;
 
-    // Add your content for the second panel here
+        Camera* camera = map_renderer->GetCamera();
+        float position[3];
+        XMStoreFloat3(reinterpret_cast<XMFLOAT3*>(position), camera->GetPosition());
+        ImGui::SliderFloat3("##position", position, -100000, 100000);
+        camera->SetPosition(position[0], position[1], position[2]);
+        ImGui::SliderFloat("Walk speed", &camera->m_walk_speed, 0.0f, 1000.0f);
+        ImGui::SliderFloat("Strafe speed", &camera->m_strafe_speed, 0.0f, 1000.0f);
 
+        float yaw = camera->GetYaw() * 180 / XM_PI;
+        float pitch = camera->GetPitch() * 180 / XM_PI;
+
+        if (ImGui::SliderFloat("Pitch", &pitch, -90.0f, 90.0f))
+        {
+            camera_projection_settings_changed = true;
+        }
+        if (ImGui::SliderFloat("Yaw", &yaw, -179.999f, 180.0f))
+        {
+            camera_projection_settings_changed = true;
+        }
+
+        auto camera_type = camera->GetCameraType();
+        float fovY = camera->GetFovY() * 180 / XM_PI;
+        float aspect_ratio = camera->GetAspectRatio();
+        float near_z = camera->GetNearZ();
+        float far_z = camera->GetFarZ();
+        float frustum_width = camera->GetViewWidth();
+        float frustum_height = camera->GetViewHeight();
+
+        if (camera_type == CameraType::Perspective)
+        {
+            if (ImGui::SliderFloat("Vertical FoV", &fovY, 1, 179))
+            {
+                camera_projection_settings_changed = true;
+            }
+        }
+
+        if (camera_type == CameraType::Orthographic)
+        {
+            static bool lock_aspect_ratio = true;
+            ImGui::Checkbox("Lock aspect ratio", &lock_aspect_ratio);
+
+            if (ImGui::SliderFloat("Frustum width", &frustum_width, 1, 300000))
+            {
+                camera_projection_settings_changed = true;
+                if (lock_aspect_ratio)
+                {
+                    frustum_height = frustum_width / aspect_ratio;
+                }
+            }
+
+            if (! lock_aspect_ratio && ImGui::SliderFloat("Frustum height", &frustum_height, 1, 300000))
+            {
+                camera_projection_settings_changed = true;
+            }
+        }
+
+        if (ImGui::SliderFloat("Near frustum z-plane", &near_z, 100, 100000))
+        {
+            camera_projection_settings_changed = true;
+        }
+
+        if (ImGui::SliderFloat("Far frustum z-plane", &far_z, near_z, near_z + 100000))
+        {
+            camera_projection_settings_changed = true;
+        }
+
+        if (camera_projection_settings_changed)
+        {
+            if (camera_type == CameraType::Perspective)
+            {
+                camera->SetFrustumAsPerspective(fovY * XM_PI / 180, aspect_ratio, near_z, far_z);
+            }
+            else
+            {
+                camera->SetFrustumAsOrthographic(frustum_width, frustum_height, near_z, far_z);
+            }
+            camera->SetOrientation(pitch * XM_PI / 180, yaw * XM_PI / 180);
+        }
+
+        if (camera_type == CameraType::Perspective)
+        {
+            if (ImGui::Button("Change to orthographic", ImVec2(-FLT_MIN, 0.0f)))
+            {
+                auto pos = camera->GetPosition();
+                camera->SetFrustumAsOrthographic(frustum_width, frustum_height, near_z, far_z);
+                camera->SetOrientation(-90.0f * XM_PI / 180, 0 * XM_PI / 180);
+            }
+        }
+        else
+        {
+            if (ImGui::Button("Change to perspective", ImVec2(-FLT_MIN, 0.0f)))
+            {
+                camera->SetFrustumAsPerspective(fovY * XM_PI / 180, aspect_ratio, near_z, far_z);
+                camera->SetOrientation(pitch * XM_PI / 180, yaw * XM_PI / 180);
+            }
+        }
+    }
     ImGui::End();
     ImGui::PopStyleVar();
 }
