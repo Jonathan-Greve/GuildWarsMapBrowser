d9c178fa0caf0650e24f55ba521a3f8924399314
Jonathan-Greve <jonagrev@gmail.com>
2023-03-23 07:48:15 +0800

    Started work on parsing model files

--- SourceFiles/FFNAType.h
+++ SourceFiles/FFNAType.h
@@ -1,4 +1,19 @@
 #pragma once
+struct GeneralChunk
+{
+    uint32_t chunk_id;
+    uint32_t chunk_size;
+    std::vector<uint8_t> chunk_data;
+
+    GeneralChunk() = default;
+    GeneralChunk(int offset, const unsigned char* data)
+    {
+        std::memcpy(&chunk_id, &data[offset], sizeof(chunk_id));
+        std::memcpy(&chunk_size, &data[offset + 4], sizeof(chunk_size));
+        chunk_data.resize(chunk_size);
+        std::memcpy(chunk_data.data(), &data[offset + 8], chunk_size);
+    }
+};
 
 enum FFNAType : unsigned char
 {
