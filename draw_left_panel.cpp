7bdcb47601247818677fbdfe9028ffa1461f8124
Jonathan-Greve <jonagrev@gmail.com>
2023-03-24 21:40:46 +0800

    Load most models when a map is selected. Crashes on maps that contains some models.

--- draw_left_panel.cpp
+++ draw_left_panel.cpp
@@ -7,10 +7,13 @@
 #include "draw_terrain_chunk.h"
 #include "draw_map_info_chunk.h"
 #include "draw_chunk_20000000.h"
+#include "draw_prop_model_info.h"
+#include "draw_dat_browser.h"
 
 extern FileType selected_file_type;
 extern FFNA_ModelFile selected_ffna_model_file;
 extern FFNA_MapFile selected_ffna_map_file;
+extern std::vector<std::pair<FileType, FileData>> selected_map_files;
 
 void draw_left_panel(MapRenderer* map_renderer)
 {
@@ -22,7 +25,79 @@ void draw_left_panel(MapRenderer* map_renderer)
     ImGui::PushStyleVar(
       ImGuiStyleVar_WindowPadding,
       ImVec2(GuiGlobalConstants::panel_padding, GuiGlobalConstants::panel_padding)); // add padding
-    ImGui::Begin("File info", NULL, window_flags);
+    float window_height = 0;
+    if (ImGui::Begin("File info", NULL, window_flags))
+    {
+
+        switch (selected_file_type)
+        {
+        case NONE:
+            break;
+        case AMAT:
+            break;
+        case AMP:
+            break;
+        case ATEXDXT1:
+            break;
+        case ATEXDXT2:
+            break;
+        case ATEXDXT3:
+            break;
+        case ATEXDXT4:
+            break;
+        case ATEXDXT5:
+            break;
+        case ATEXDXTN:
+            break;
+        case ATEXDXTA:
+            break;
+        case ATEXDXTL:
+            break;
+        case ATTXDXT1:
+            break;
+        case ATTXDXT3:
+            break;
+        case ATTXDXT5:
+            break;
+        case ATTXDXTN:
+            break;
+        case ATTXDXTA:
+            break;
+        case ATTXDXTL:
+            break;
+        case DDS:
+            break;
+        case FFNA_Type2:
+            break;
+        case FFNA_Type3:
+            draw_chunk_20000000(selected_ffna_map_file.chunk1);
+            draw_map_info_chunk(selected_ffna_map_file.map_info_chunk);
+            draw_props_info_panel(selected_ffna_map_file.props_info_chunk);
+            draw_props_filenames_panel(selected_ffna_map_file.prop_filenames_chunk);
+            //draw_chunk_20000003(selected_ffna_map_file.chunk5);
+            draw_terrain_chunk(selected_ffna_map_file.terrain_chunk);
+            break;
+        case FFNA_Unknown:
+            break;
+        case MFTBASE:
+            break;
+        case NOTREAD:
+            break;
+        case SOUND:
+            break;
+        case TEXT:
+            break;
+        case UNKNOWN:
+            break;
+        default:
+            break;
+        }
+        window_height = ImGui::GetWindowSize().y;
+    }
+    ImGui::End();
+
+    float max_window_height = ImGui::GetIO().DisplaySize.y - window_height -
+      (3 * GuiGlobalConstants::panel_padding); // Calculate max height based on app window size and padding
 
     switch (selected_file_type)
     {
@@ -65,12 +140,40 @@ void draw_left_panel(MapRenderer* map_renderer)
     case FFNA_Type2:
         break;
     case FFNA_Type3:
-        draw_chunk_20000000(selected_ffna_map_file.chunk1);
-        draw_map_info_chunk(selected_ffna_map_file.map_info_chunk);
-        draw_props_info_panel(selected_ffna_map_file.props_info_chunk);
-        draw_props_filenames_panel(selected_ffna_map_file.prop_filenames_chunk);
-        //draw_chunk_20000003(selected_ffna_map_file.chunk5);
-        draw_terrain_chunk(selected_ffna_map_file.terrain_chunk);
+        ImGui::SetNextWindowPos(
+          ImVec2(GuiGlobalConstants::panel_padding,
+                 GuiGlobalConstants::panel_padding + window_height + GuiGlobalConstants::panel_padding));
+        ImGui::SetNextWindowSize(ImVec2(GuiGlobalConstants::left_panel_width, 0));
+        ImGui::SetNextWindowSizeConstraints(ImVec2(0, 0),
+                                            ImVec2(GuiGlobalConstants::left_panel_width, max_window_height));
+        if (selected_map_files.size() > 0)
+        {
+            if (ImGui::Begin("Prop models", NULL, window_flags))
+            {
+
+                int i = 0;
+                int k = 0;
+                for (const auto& [file_type, file_data] : selected_map_files)
+                {
+                    i++;
+                    if (file_type == FFNA_Type2)
+                    {
+                        k++;
+                        if (auto ffna_model_file_ptr = std::get_if<FFNA_ModelFile>(&file_data))
+                        {
+                            // Use *ffna_model_file_ptr to access the FFNA_ModelFile data
+                            auto id = std::format("({}) {}", i, k);
+                            if (ImGui::TreeNode(id.c_str()))
+                            {
+                                draw_prop_model_info(*ffna_model_file_ptr);
+                                ImGui::TreePop();
+                            }
+                        }
+                    }
+                }
+            }
+            ImGui::End();
+        }
         break;
     case FFNA_Unknown:
         break;
@@ -88,6 +191,5 @@ void draw_left_panel(MapRenderer* map_renderer)
         break;
     }
 
-    ImGui::End();
     ImGui::PopStyleVar();
 }
