d8b7785b9ea95286e95f198763284bbdb5fd73e8
Jonathan-Greve <jonagrev@gmail.com>
2023-03-29 08:35:21 +0800

    Added new info to prop info panel

--- draw_props_info_panel.cpp
+++ draw_props_info_panel.cpp
@@ -23,7 +23,16 @@ void draw_props_info_panel(const Chunk3& chunk)
                 {
                     ImGui::Text("model filename index: %hu", prop.filename_index);
                     ImGui::Text("Position: (%f, %f, %f)", prop.x, prop.y, prop.z);
-                    // Add other properties of PropInfo here...
+                    ImGui::Text("f4: %f", prop.f4);
+                    ImGui::Text("f5: %f", prop.f5);
+                    ImGui::Text("f6: %f", prop.f6);
+                    ImGui::Text("cos_angle: %f", prop.cos_angle);
+                    ImGui::Text("sin_angle: %f", prop.sin_angle);
+                    ImGui::Text("f9: %f", prop.f9);
+                    ImGui::Text("scaling_factor: %f", prop.scaling_factor);
+                    ImGui::Text("f11: %f", prop.f11);
+                    ImGui::Text("f12: %hhu", prop.f12);
+                    ImGui::Text("num_some_structs: %hhu", prop.num_some_structs);
 
                     if (ImGui::TreeNode("Some Structs"))
                     {
