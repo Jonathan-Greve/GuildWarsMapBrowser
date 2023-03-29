9fd0c2f3d8bf56ad0c37b48d8bcdcfc8b4bfe7af
Jonathan-Greve <jonagrev@gmail.com>
2023-03-29 08:40:31 +0800

    Fixed rotation

--- SourceFiles/draw_dat_browser.cpp
+++ SourceFiles/draw_dat_browser.cpp
@@ -97,14 +97,13 @@ void parse_file(DATManager& dat_manager, int index, MapRenderer* map_renderer)
                         float cos_angle = prop_info.cos_angle;
                         float sin_angle = prop_info.sin_angle;
                         float rotation_angle = std::atan2(
-                          sin_angle,
-                          cos_angle); // Calculate the rotation angle from the sine and cosine values
+                          cos_angle,
+                          sin_angle); // Calculate the rotation angle from the sine and cosine values
 
                         float scale = prop_info.scaling_factor;
 
                         DirectX::XMMATRIX scaling_matrix = DirectX::XMMatrixScaling(scale, scale, scale);
-                        DirectX::XMMATRIX rotation_matrix =
-                          DirectX::XMMatrixRotationY(std::acos(cos_angle) + XM_PI / 2.0f);
+                        DirectX::XMMATRIX rotation_matrix = DirectX::XMMatrixRotationY(rotation_angle);
                         DirectX::XMMATRIX translation_matrix =
                           DirectX::XMMatrixTranslationFromVector(XMLoadFloat3(&translation));
 
