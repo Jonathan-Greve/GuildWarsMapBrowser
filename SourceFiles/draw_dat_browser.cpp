a7785615bc86ede816364e07e33a53cf1271c0b9
Jonathan-Greve <jonagrev@gmail.com>
2023-04-01 21:41:27 +0800

    Load all models before settings props to render.

--- SourceFiles/draw_dat_browser.cpp
+++ SourceFiles/draw_dat_browser.cpp
@@ -140,6 +140,25 @@ void parse_file(DATManager& dat_manager, int index, MapRenderer* map_renderer,
                 }
             }
         }
+
+        // Load models
+        for (int i = 0; i < selected_ffna_map_file.more_filnames_chunk.array.size(); i++)
+        {
+            auto decoded_filename =
+              decode_filename(selected_ffna_map_file.more_filnames_chunk.array[i].filename.id0,
+                              selected_ffna_map_file.more_filnames_chunk.array[i].filename.id1);
+            auto mft_entry_it = hash_index.find(decoded_filename);
+            if (mft_entry_it != hash_index.end())
+            {
+                auto type = items[mft_entry_it->second.at(0)].type;
+                if (type == FFNA_Type2)
+                {
+                    auto map_model = dat_manager.parse_ffna_model_file(mft_entry_it->second.at(0));
+                    selected_map_files.emplace_back(map_model);
+                }
+            }
+        }
+
         for (int i = 0; i < selected_ffna_map_file.props_info_chunk.prop_array.props_info.size(); i++)
         {
             PropInfo prop_info = selected_ffna_map_file.props_info_chunk.prop_array.props_info[i];
@@ -183,28 +202,6 @@ void parse_file(DATManager& dat_manager, int index, MapRenderer* map_renderer,
 
                         auto mesh_ids = map_renderer->AddProp(prop_meshes, per_object_cb, i);
                     }
-                }
-            }
-        }
-
-        // Load models
-        for (int i = 0; i < selected_ffna_map_file.more_filnames_chunk.array.size(); i++)
-        {
-            auto decoded_filename =
-              decode_filename(selected_ffna_map_file.more_filnames_chunk.array[i].filename.id0,
-                              selected_ffna_map_file.more_filnames_chunk.array[i].filename.id1);
-            auto mft_entry_it = hash_index.find(decoded_filename);
-            if (mft_entry_it != hash_index.end())
-            {
-                auto type = items[mft_entry_it->second.at(0)].type;
-                if (type == FFNA_Type2)
-                {
-                    auto map_model = dat_manager.parse_ffna_model_file(mft_entry_it->second.at(0));
-                    selected_map_files.emplace_back(map_model);
-                }
-                else
-                {
-                    selected_map_files.emplace_back(FFNA_ModelFile());
                 }
             }
         }
