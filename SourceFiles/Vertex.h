4e45c6dfac5fc5cc57064c88cb56dfc8decda3b7
Jonathan-Greve <jonagrev@gmail.com>
2023-04-05 07:44:41 +0800

    Added functions for converting GWFVF to the actual Dx9 FVF. So we have Dat_FVF -> GW_FVF -> ActualFVF

--- SourceFiles/Vertex.h
+++ SourceFiles/Vertex.h
@@ -15,3 +15,54 @@ inline extern D3D11_INPUT_ELEMENT_DESC inputLayoutDesc[] = {
   {"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(Vertex, normal), D3D11_INPUT_PER_VERTEX_DATA, 0},
   {"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, offsetof(Vertex, tex_coord), D3D11_INPUT_PER_VERTEX_DATA, 0},
 };
+
+inline int get_most_significant_bit_pos(uint32_t value)
+{
+    int bit_position = 31; // For 32-bit unsigned integer, highest bit position is 31
+    if (value != 0)
+    {
+        while ((value >> bit_position) == 0)
+        {
+            bit_position--;
+        }
+    }
+    return bit_position;
+}
+
+inline uint32_t FVF_to_ActualFVF(uint32_t FVF)
+{
+    int most_significant_bit_index;
+    uint32_t ActualFVF;
+    uint32_t currFlag;
+
+    uint32_t FVF_to_ActualFVF_converter_array[] = {
+      0x00000003, 0x00008006, 0x00000001, 0x00000002, 0x00000004, 0x00000010, 0x00000010, 0x00010000,
+      0x00000020, 0x00020000, 0x00000008, 0x00000040, 0x00000040, 0x00040000, 0x00000000, 0x00000000};
+
+    ActualFVF = 0;
+    assert((FVF & 0x40) == 0 || (FVF & 0x34) == 0);
+
+    if ((FVF & 0xff00) != 0)
+    {
+        most_significant_bit_index = get_most_significant_bit_pos(FVF & 0xff00);
+        ActualFVF = (most_significant_bit_index - 7) << 8;
+        FVF = FVF & 0xffff00ff;
+    }
+    if (FVF != 0)
+    {
+        const uint32_t* pArray = &FVF_to_ActualFVF_converter_array[1];
+        do
+        {
+            currFlag = *(pArray - 1);
+            assert(currFlag != 0);
+
+            if ((currFlag & FVF) == currFlag)
+            {
+                FVF = FVF & ~currFlag;
+                ActualFVF = ActualFVF | *pArray;
+            }
+            pArray = pArray + 2;
+        } while (FVF != 0);
+    }
+    return ActualFVF;
+}
