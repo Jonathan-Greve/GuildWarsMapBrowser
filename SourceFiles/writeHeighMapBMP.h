590bbd7b265e44f88106c0e022fb8f5d19db9aa4
Jonathan-Greve <jonagrev@gmail.com>
2023-08-22 21:38:20 +0800

    Can now write tiff files and can export terrain texture indices and some unknown per tile data for the terrain (looks like some shadow stuff maybe?)

--- SourceFiles/writeHeighMapBMP.h
+++ SourceFiles/writeHeighMapBMP.h
@@ -4,4 +4,4 @@
 
 bool write_heightmap_png(const std::vector<std::vector<float>>& heightmap, const char* filename);
 bool write_heightmap_tiff(const std::vector<std::vector<float>>& heightmap, const char* filename);
-
+bool write_terrain_ints_tiff(const std::vector<std::vector<uint32_t>>& terrain_indices, const char* filename);
