76487c4ba174a1cbf4398bac0562dec0f6fc9709
Jonathan-Greve <jonagrev@gmail.com>
2023-09-14 14:27:40 +0800

    Showing more prop info

--- SourceFiles/draw_props_filenames_panel.h
+++ SourceFiles/draw_props_filenames_panel.h
@@ -1,4 +1,22 @@
 #pragma once
 #include "FFNA_MapFile.h"
+#include "FFNA_ModelFile.h"
+
+inline void draw_prop_filename_element(size_t i, const Chunk4DataElement& element, bool default_open = false)
+{
+	auto flag = ImGuiTreeNodeFlags_None;
+	if (default_open) flag = ImGuiTreeNodeFlags_DefaultOpen;
+	if (ImGui::TreeNodeEx((std::string("Element #") + std::to_string(i)).c_str(), flag))
+	{
+		ImGui::Text("f1: %hu", element.f1);
+		ImGui::Text("File Name ID0: %hu", element.filename.id0);
+		ImGui::Text("File Name ID1: %hu", element.filename.id1);
+
+		int decoded_hash = decode_filename(element.filename.id0, element.filename.id1);
+		const auto file_hash_text = std::format("0x{:X} ({})", decoded_hash, decoded_hash);
+		ImGui::Text("File hash: %s", file_hash_text.c_str());
+		ImGui::TreePop();
+	}
+}
 
 void draw_props_filenames_panel(const Chunk4& chunk);
--- SourceFiles/draw_props_info_panel.h
+++ SourceFiles/draw_props_info_panel.h
@@ -2,3 +2,36 @@
 #include "FFNA_MapFile.h"
 
 void draw_props_info_panel(const Chunk3& chunk);
+
+inline void draw_prop_info(const PropInfo& prop, bool default_open = false)
+{
+	ImGui::Text("model filename index: %hu", prop.filename_index);
+	ImGui::Text("Position: (%f, %f, %f)", prop.x, prop.y, prop.z);
+	ImGui::Text("f4: %f", prop.f4);
+	ImGui::Text("f5: %f", prop.f5);
+	ImGui::Text("f6: %f", prop.f6);
+	ImGui::Text("sin_angle: %f", prop.sin_angle);
+	ImGui::Text("cos_angle: %f", prop.cos_angle);
+	ImGui::Text("f9: %f", prop.f9);
+	ImGui::Text("scaling_factor: %f", prop.scaling_factor);
+	ImGui::Text("f11: %f", prop.f11);
+	ImGui::Text("f12: %hhu", prop.f12);
+	ImGui::Text("num_some_structs: %hhu", prop.num_some_structs);
+
+	auto flag = ImGuiTreeNodeFlags_None;
+	if (default_open) flag = ImGuiTreeNodeFlags_DefaultOpen;
+	if (ImGui::TreeNodeEx("Some Structs", flag))
+	{
+		for (size_t j = 0; j < prop.some_structs.size(); j += 8)
+		{
+			ImGui::Text("Some Struct #%zu", j / 8);
+			ImGui::Text("Data: ");
+			for (int k = 0; k < 8; ++k)
+			{
+				ImGui::SameLine();
+				ImGui::Text("%02X", prop.some_structs[j + k]);
+			}
+		}
+		ImGui::TreePop();
+	}
+}
