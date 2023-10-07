094f75f0603e4971414e6a1bc0d4b613a440187f
Jonathan-Greve <jonagrev@gmail.com>
2023-10-07 20:07:06 +0800

    Using raw mouse input for a much smoother and bug free camera movement experience

--- SourceFiles/InputManager.cpp
+++ SourceFiles/InputManager.cpp
@@ -1,10 +1,34 @@
 #include "pch.h"
 #include "InputManager.h"
 
-InputManager::InputManager()
-	: m_mouse_pos{0, 0} {}
+InputManager::InputManager(HWND hWnd)
+	: m_mouse_pos{0, 0}
+{
+	rid.usUsagePage = 0x01; // Generic Desktop Controls
+    rid.usUsage = 0x02;    // Mouse
+    rid.dwFlags = 0;
+    rid.hwndTarget = hWnd; // Handle to your window
+    RegisterRawInputDevices(&rid, 1, sizeof(rid));
+}
 
 InputManager::~InputManager() { }
+
+void InputManager::ProcessRawInput(LPARAM lParam)
+{
+    UINT dwSize = sizeof(RAWINPUT);
+    static BYTE lpb[sizeof(RAWINPUT)];
+
+    GetRawInputData((HRAWINPUT)lParam, RID_INPUT, lpb, &dwSize, sizeof(RAWINPUTHEADER));
+    RAWINPUT* raw = (RAWINPUT*)lpb;
+
+    if (raw->header.dwType == RIM_TYPEMOUSE) 
+    {
+        int dx = raw->data.mouse.lLastX;
+        int dy = raw->data.mouse.lLastY;
+
+        OnRawMouseMove(dx, dy);
+    }
+}
 
 void InputManager::AddMouseMoveListener(MouseMoveListener* listener) { m_mouseMoveListeners.push_back(listener); }
 
@@ -14,28 +38,29 @@ void InputManager::OnKeyDown(WPARAM wParam, HWND hWnd) { }
 
 void InputManager::OnKeyUp(WPARAM wParam, HWND hWnd) { }
 
-void InputManager::OnMouseMove(int x, int y, WPARAM wParam, HWND hWnd)
+void InputManager::OnRawMouseMove(int dx, int dy)
 {
-	if ((wParam & MK_RBUTTON) != 0)
-	{
-		// Make each pixel correspond to a quarter of a degree.
-		float dx = DirectX::XMConvertToRadians(0.25f * static_cast<float>(x - m_mouse_pos.x));
-		float dy = DirectX::XMConvertToRadians(0.25f * static_cast<float>(y - m_mouse_pos.y));
+    if (m_isCursorShown) return; // Ignore if cursor is shown
 
-		// Call listeners with dx, dy data
-		for (MouseMoveListener* listener : m_mouseMoveListeners) { listener->OnMouseMove(dx, dy); }
-	}
+    float radianX = DirectX::XMConvertToRadians(0.25f * dx);
+    float radianY = DirectX::XMConvertToRadians(0.25f * dy);
 
-	m_mouse_pos.x = x;
-	m_mouse_pos.y = y;
+    for (MouseMoveListener* listener : m_mouseMoveListeners)
+    {
+        listener->OnMouseMove(radianX, radianY);
+    }
+
+	SetCursorPos(m_right_mouse_button_down_pos.x, m_right_mouse_button_down_pos.y);
 }
 
 void InputManager::OnMouseDown(int x, int y, WPARAM wParam, HWND hWnd, UINT message)
 {
+	TrackMouseLeave(hWnd);
+
 	m_mouse_pos.x = x;
 	m_mouse_pos.y = y;
 
-	if (message & WM_RBUTTONDOWN)
+	if (message == WM_RBUTTONDOWN)
 	{
 		if (m_isCursorShown)
 		{
@@ -46,6 +71,9 @@ void InputManager::OnMouseDown(int x, int y, WPARAM wParam, HWND hWnd, UINT mess
 
 		// Get the current cursor position
 		GetCursorPos(&m_right_mouse_button_down_pos);
+
+		POINT center = GetWindowCenter(hWnd);
+        SetCursorPos(center.x, center.y);
 	}
 
 	SetCapture(hWnd);
@@ -53,7 +81,7 @@ void InputManager::OnMouseDown(int x, int y, WPARAM wParam, HWND hWnd, UINT mess
 
 void InputManager::OnMouseUp(int x, int y, WPARAM wParam, HWND hWnd, UINT message)
 {
-	if (message & WM_RBUTTONUP)
+	if (message == WM_RBUTTONUP)
 	{
 		// Set the cursor position to the stored position
 		SetCursorPos(m_right_mouse_button_down_pos.x, m_right_mouse_button_down_pos.y);
@@ -86,6 +114,25 @@ void InputManager::OnMouseWheel(short wheel_delta, HWND hWnd)
 	}
 }
 
+void InputManager::OnMouseLeave(HWND hWnd)
+{
+    if (!m_isCursorShown)
+    {
+        // Show the cursor
+        ShowCursor(TRUE);
+        m_isCursorShown = true;
+    }
+}
+
+void InputManager::TrackMouseLeave(HWND hWnd)
+{
+    TRACKMOUSEEVENT tme;
+    tme.cbSize = sizeof(tme);
+    tme.dwFlags = TME_LEAVE;
+    tme.hwndTrack = hWnd;
+    TrackMouseEvent(&tme);
+}
+
 bool InputManager::IsKeyDown(UINT key) const { return (GetAsyncKeyState(key) & 0x8000); }
 
 POINT InputManager::GetClientCoords(HWND hWnd)
@@ -100,3 +147,15 @@ POINT InputManager::GetClientCoords(HWND hWnd)
 
 	return screenPoint;
 }
+
+POINT InputManager::GetWindowCenter(HWND hWnd)
+{
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    POINT center;
+    center.x = (rect.right - rect.left) / 2;
+    center.y = (rect.bottom - rect.top) / 2;
+    ClientToScreen(hWnd, &center);
+    return center;
+}
+
--- SourceFiles/InputManager.h
+++ SourceFiles/InputManager.h
@@ -4,15 +4,17 @@
 class InputManager
 {
 public:
-    InputManager();
+    InputManager(HWND hWnd);
     ~InputManager();
+
+    void ProcessRawInput(LPARAM lParam);
 
     void AddMouseMoveListener(MouseMoveListener* listener);
     void RemoveMouseMoveListener(MouseMoveListener* listener);
 
     void OnKeyDown(WPARAM wParam, HWND hWnd);
     void OnKeyUp(WPARAM wParam, HWND hWnd);
-    void OnMouseMove(int x, int y, WPARAM wParam, HWND hWnd);
+    void OnRawMouseMove(int dx, int dy);
 
     void OnMouseDown(int x, int y, WPARAM wParam, HWND hWnd, UINT message);
 
@@ -22,12 +24,21 @@ public:
 
     bool IsKeyDown(UINT key) const;
 
+    void TrackMouseLeave(HWND hWnd);
+	void OnMouseLeave(HWND hWnd);
+
     POINT GetClientCoords(HWND hWnd);
 
+	POINT GetWindowCenter(HWND hWnd);
+
 private:
+    RAWINPUTDEVICE rid;
+
     POINT m_mouse_pos;
     POINT m_right_mouse_button_down_pos;
     std::vector<MouseMoveListener*> m_mouseMoveListeners;
+    bool m_isSettingCursorPosition = false;
+
 
     bool m_isCursorShown = true;
 };
