45c0fc8ac133fdf3063d0567b6d0877f67c4e5b6
Jonathan-Greve <jonagrev@gmail.com>
2023-10-19 08:05:33 +0800

    Lowered default specular intensity.

--- SourceFiles/MapRenderer.h
+++ SourceFiles/MapRenderer.h
@@ -49,7 +49,7 @@ public:
         // Setup lighting
         m_directionalLight.ambient = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
         m_directionalLight.diffuse = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
-        m_directionalLight.specular = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
+        m_directionalLight.specular = XMFLOAT4(0.1f, 0.1f, 0.1f, 1.0f);
         m_directionalLight.direction = XMFLOAT3(1.0f, -1.0f, 0.0f);
         m_directionalLight.pad = 0.0f;
 
