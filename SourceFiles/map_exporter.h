d928a18cb4727c14f92561ab009f5399d124dce4
Jonathan Bjørn Greve <jonagrev@gmail.com>
2023-11-03 23:16:52 +0800

    Fixed paths for exporting maps and models to json

--- SourceFiles/map_exporter.h
+++ SourceFiles/map_exporter.h
@@ -26,9 +26,11 @@ struct gwmb_terrain
     std::vector<gwmb_texture> textures; // A terrain has up to 64 textures.
 };
 
-struct gwmb_map_models
+// Each model is exported as it's own separate .json file
+// We use the model_hash to identify which transforms should be applied to the model.
+struct gwmb_map_model
 {
-    gwmb_model model;
+    int model_hash;
     gwmb_vec3f world_pos; // To translate model into its world position.
     gwmb_vec3f model_up; // The models up vector. Used for rotation.
     gwmb_vec3f model_look; // THe models look vector. Used for rotation.
@@ -37,8 +39,9 @@ struct gwmb_map_models
 
 struct gwmb_map
 {
+    int filehash;
     gwmb_terrain terrain;
-    std::vector<gwmb_map_models> models;
+    std::vector<gwmb_map_model> models;
 };
 
 namespace nlohmann {
@@ -76,18 +79,18 @@ namespace nlohmann {
     };
 
     template<>
-    struct adl_serializer<gwmb_map_models> {
-        static void to_json(json& j, const gwmb_map_models& mm) {
+    struct adl_serializer<gwmb_map_model> {
+        static void to_json(json& j, const gwmb_map_model& mm) {
             j = json{
-                {"model", mm.model},
+                {"model_hash", mm.model_hash},
                 {"world_pos", mm.world_pos},
                 {"model_up", mm.model_up},
                 {"model_look", mm.model_look},
                 {"scale", mm.scale}
             };
         }
-        static void from_json(const json& j, gwmb_map_models& mm) {
-            j.at("model").get_to(mm.model);
+        static void from_json(const json& j, gwmb_map_model& mm) {
+            j.at("model_hash").get_to(mm.model_hash);
             j.at("world_pos").get_to(mm.world_pos);
             j.at("model_up").get_to(mm.model_up);
             j.at("model_look").get_to(mm.model_look);
@@ -96,7 +99,7 @@ namespace nlohmann {
     };
 
     template<>
-        struct adl_serializer<gwmb_map> {
+    struct adl_serializer<gwmb_map> {
         static void to_json(json& j, const gwmb_map& m) {
             j = json{
                 {"terrain", m.terrain},
@@ -114,33 +117,30 @@ namespace nlohmann {
 class map_exporter
 {
 public:
-    static bool export_map(const std::string& save_path, const int map_mft_index, DATManager& dat_manager, std::unordered_map<int, std::vector<int>>& hash_index, TextureManager* texture_manager, const bool json_pretty_print = false) {
+    static bool export_map(const std::string& save_directory, const int map_filehash, const int map_mft_index, DATManager& dat_manager, std::unordered_map<int, std::vector<int>>& hash_index, TextureManager* texture_manager, const bool json_pretty_print = false) {
         // Build model
         gwmb_map map;
-        const bool success = generate_gwmb_map(map, map_mft_index, dat_manager, hash_index, texture_manager);
+        const bool success = generate_gwmb_map(save_directory, map, map_mft_index, dat_manager, hash_index, texture_manager);
         if (!success)
             return false;
 
         const nlohmann::json j = map;
 
-        std::ofstream file(save_path);
+        std::filesystem::path file_path = std::filesystem::path(save_directory) / ("map_" + std::to_string(map_filehash) + ".json");
+
+        std::ofstream file(file_path);
         if (!file) {
-            return false; // Failed to open the file
+            return false;
         }
 
-        if (json_pretty_print) {
-            file << j.dump(4);
-        }
-        else {
-            file << j.dump();
-        }
+        file << (json_pretty_print ? j.dump(4) : j.dump());
         file.close();
 
         return true; // Successfully exported the model
     }
 
 private:
-    static bool generate_gwmb_map(gwmb_map& map, int map_mft_index, DATManager& dat_manager, std::unordered_map<int, std::vector<int>>& hash_index, TextureManager* texture_manager) {
+    static bool generate_gwmb_map(const std::string& save_directory, gwmb_map& map, int map_mft_index, DATManager& dat_manager, std::unordered_map<int, std::vector<int>>& hash_index, TextureManager* texture_manager) {
         auto map_file = dat_manager.parse_ffna_map_file(map_mft_index);
 
         if (map_file.terrain_chunk.terrain_heightmap.size() > 0 &&
@@ -209,18 +209,82 @@ private:
                 new_gwmb_map_vertex.normal = { vertex.normal.x, vertex.normal.y, vertex.normal.z };
                 new_gwmb_map_vertex.pos = { vertex.position.x, vertex.position.y, vertex.position.z };
 
-                new_gwmb_map_vertex.uv_coord = { vertex.tex_coord0.x , vertex.tex_coord0.y};
+                new_gwmb_map_vertex.uv_coord = { vertex.tex_coord0.x , vertex.tex_coord0.y };
 
                 new_terrain.vertices[i] = new_gwmb_map_vertex;
             }
 
             new_terrain.indices.resize(terrain->get_mesh()->indices.size());
-            for (int i = 0; i< terrain_mesh->indices.size(); i++) {
+            for (int i = 0; i < terrain_mesh->indices.size(); i++) {
                 const auto index = terrain_mesh->indices[i];
                 new_terrain.indices[i] = index;
             }
 
-            // Now add all the models to the map
+            // Now export all the models into their own separate JSON files.
+            std::vector<int> model_hashes;
+            for (int i = 0; i < map_file.prop_filenames_chunk.array.size(); i++)
+            {
+                auto decoded_filename =
+                    decode_filename(map_file.prop_filenames_chunk.array[i].filename.id0,
+                        map_file.prop_filenames_chunk.array[i].filename.id1);
+                auto mft_entry_it = hash_index.find(decoded_filename);
+                if (mft_entry_it != hash_index.end())
+                {
+                    // Export model to map folder
+                    model_exporter::export_model(std::format("{}\\model_0x{:X}_gwmb.json", save_directory, decoded_filename), mft_entry_it->second.at(0), dat_manager, hash_index, texture_manager);
+                    model_hashes.push_back(decoded_filename);
+                }
+            }
+
+            for (int i = 0; i < map_file.more_filnames_chunk.array.size(); i++)
+            {
+                auto decoded_filename =
+                    decode_filename(map_file.more_filnames_chunk.array[i].filename.id0,
+                        map_file.more_filnames_chunk.array[i].filename.id1);
+                auto mft_entry_it = hash_index.find(decoded_filename);
+                if (mft_entry_it != hash_index.end())
+                {
+                    // Export model to map folder
+                    model_exporter::export_model(std::format("{}\\model_0x{:X}_gwmb.json", save_directory, decoded_filename), mft_entry_it->second.at(0), dat_manager, hash_index, texture_manager);
+                    model_hashes.push_back(decoded_filename);
+                }
+            }
+
+            // Finally add the model transform info to the map
+            for (int i = 0; i < map_file.props_info_chunk.prop_array.props_info.size(); i++)
+            {
+                PropInfo prop_info = map_file.props_info_chunk.prop_array.props_info[i];
+                const int model_filename_index = prop_info.filename_index;
+                const int model_hash = model_hashes[model_filename_index];
+
+                // rotation
+                XMFLOAT3 translation(prop_info.x, prop_info.y, prop_info.z);
+
+                XMFLOAT3 vec1{ prop_info.f4, -prop_info.f6, prop_info.f5 };
+                XMFLOAT3 vec2{ prop_info.sin_angle, -prop_info.f9, prop_info.cos_angle };
+
+                // Load vectors into XMVECTORs
+                XMVECTOR v2 = XMLoadFloat3(&vec1);
+                XMVECTOR v3 = XMLoadFloat3(&vec2);
+
+                // Compute the third orthogonal vector with cross product
+                // Note: This is for left-handed coordinate systems
+                XMVECTOR v1 = XMVector3Cross(v3, v2);
+
+                // Ensure all vectors are normalized
+                v1 = XMVector3Normalize(v1); // Right
+                v2 = XMVector3Normalize(v2); // Up
+                v3 = XMVector3Normalize(v3); // Look
+
+                gwmb_map_model new_map_model;
+                new_map_model.model_hash = model_hash;
+                new_map_model.scale = prop_info.scaling_factor;
+                new_map_model.world_pos = { prop_info.x, prop_info.y, prop_info.z };
+                new_map_model.model_up = { v2.m128_f32[0], v2.m128_f32[1], v2.m128_f32[2] };
+                new_map_model.model_look = { -v3.m128_f32[0], -v3.m128_f32[1], v3.m128_f32[2] };
+
+                map.models.push_back(new_map_model);
+            }
 
             map.terrain = new_terrain;
         }
