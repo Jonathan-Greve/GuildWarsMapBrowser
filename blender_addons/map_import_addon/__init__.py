9eb0c7c858814c6bcfbea7f4744861fdd341b912
Jonathan Bjørn Greve <jonagrev@gmail.com>
2023-11-14 01:56:13 +0800

    Fixed map addon not being able to activate due to wrong import and method name

--- blender_addons/map_import_addon/__init__.py
+++ blender_addons/map_import_addon/__init__.py
@@ -12,20 +12,20 @@ bl_info = {
 
 import bpy
 
-from . import import_model
+from . import import_map
 
 
 def menu_func_import(self, context):
-    self.layout.operator(import_model.IMPORT_OT_JSONMesh.bl_idname, text="GW Map Browser Model Map Folder")
+    self.layout.operator(import_map.IMPORT_OT_GWMBMap.bl_idname, text="GW Map Browser Map Folder")
 
 
 def register():
-    bpy.utils.register_class(import_model.IMPORT_OT_JSONMesh)
+    bpy.utils.register_class(import_map.IMPORT_OT_GWMBMap)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 
 def unregister():
-    bpy.utils.unregister_class(import_model.IMPORT_OT_JSONMesh)
+    bpy.utils.unregister_class(import_map.IMPORT_OT_GWMBMap)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
 
 
