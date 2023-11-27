8affa48c9bdf6debb7dee1732610430fae75ad75
Jonathan Bjørn Greve <jonagrev@gmail.com>
2023-11-27 20:48:00 +0800

    Work on dat comparison tool

--- SourceFiles/draw_dat_compare_panel.cpp
+++ SourceFiles/draw_dat_compare_panel.cpp
@@ -7,6 +7,9 @@
 
 std::vector<std::wstring> file_paths;
 std::map<std::wstring, int> filepath_to_alias; // Map to track filepath and its alias
+std::vector<std::unordered_map<uint32_t, uint32_t>> dat_fileid_to_mm3hash;
+std::vector<uint32_t> filter_eval_result;
+std::set<uint32_t> all_dats_file_ids; // all the file_ids in dat_fileid_to_mm3hash
 
 Lexer lexer{ "" };
 std::unique_ptr<Parser> parser;
@@ -61,6 +64,12 @@ void draw_dat_compare_panel(std::map<int, std::unique_ptr<DATManager>>& dat_mana
 
     bool is_analyzing = total_additional_files_read < total_additional_files;
 
+    // Show progress bar of loading additional dat files
+    // Draw a single progress bar for the cumulative progress
+    if (is_analyzing) {
+        draw_dat_load_progress_bar(total_additional_files_read, total_additional_files);
+    }
+
     ImGui::Begin("Compare DAT files");
 
     if (!is_analyzing) {
@@ -97,6 +106,7 @@ void draw_dat_compare_panel(std::map<int, std::unique_ptr<DATManager>>& dat_mana
                     file_paths.push_back(wstr);
                     filepath_to_alias[wstr] = filepath_to_alias.size();
                     add_dat_manager(wstr, dat_managers);
+                    is_analyzing = true;
                 }
             }
             ImGuiFileDialog::Instance()->Close();
@@ -127,6 +137,7 @@ void draw_dat_compare_panel(std::map<int, std::unique_ptr<DATManager>>& dat_mana
                     // Erase file from vectors and map
                     filepath_to_alias.erase(file_paths[i]);
                     file_paths.erase(file_paths.begin() + i);
+                    dat_fileid_to_mm3hash.clear();
 
                     // Reorder the aliases in the map
                     for (auto& pair : filepath_to_alias) {
@@ -166,18 +177,20 @@ void draw_dat_compare_panel(std::map<int, std::unique_ptr<DATManager>>& dat_mana
 
     if (!is_analyzing) {
         ImGui::Separator();
-        static char filter_expression[256] = ""; // Buffer for input text
+        static std::string filter_expression = ""; // Buffer for input text
 
         static std::string filter_expr_error = "";
         static std::string filter_last_success_parsed_expr = "";
+        static int num_eval_errors = 0;
+        static std::shared_ptr<ASTNode> filter_last_success_parsed_AST;
 
         ImGui::Text("Filter Expression");
         ImGui::SameLine();
-        if (ImGui::InputText("##filter_expression", filter_expression, sizeof(filter_expression), ImGuiInputTextFlags_CallbackAlways, TextEditCallback)) {
+        if (ImGui::InputText("##filter_expression", &filter_expression, ImGuiInputTextFlags_CallbackAlways, TextEditCallback)) {
             try {
                 lexer = Lexer(filter_expression);
                 parser = std::make_unique<Parser>(lexer); // Create a new Parser instance
-                auto AST = parser->parse();
+                filter_last_success_parsed_AST = parser->parse();
 
                 filter_expr_error = "";
                 filter_last_success_parsed_expr = filter_expression;
@@ -195,13 +208,71 @@ void draw_dat_compare_panel(std::map<int, std::unique_ptr<DATManager>>& dat_mana
         if (filter_last_success_parsed_expr != "" && filter_expr_error.size() > 0) {
             ImGui::Text(std::format("Last successful parse: \"{}\"", filter_last_success_parsed_expr).c_str());
         }
+
+        if (num_eval_errors > 0 || filter_eval_result.size() > 0) {
+            ImGui::Text(std::format("Num eval errors: {}", num_eval_errors).c_str());
+            ImGui::Text(std::format("Num matching files: {}", filter_eval_result.size()).c_str());
+        }
+
+        if (filter_expression.size() > 0 && filter_expression == filter_last_success_parsed_expr && filter_expr_error == "") {
+            if (ImGui::Button("Start filtering")) {
+                filter_eval_result.clear();
+                num_eval_errors = 0;
+
+                for (const auto file_id : all_dats_file_ids) {
+                    std::map<int, uint32_t> DATs_hashes;
+                    for (int i = 0; i < dat_managers.size(); i++) {
+                        const auto it = dat_fileid_to_mm3hash[i].find(file_id);
+                        if (it != dat_fileid_to_mm3hash[i].end()) {
+                            DATs_hashes.insert_or_assign(i, it->second);
+                        }
+                    }
+
+                    try
+                    {
+                        const auto eval_result = evaluate(filter_last_success_parsed_AST, DATs_hashes);
+                        filter_eval_result.insert(filter_eval_result.end(), eval_result.begin(), eval_result.end());
+                    }
+                    catch (const std::exception&)
+                    {
+                        num_eval_errors += 1;
+                    }
+                }
+            }
+        }
     }
 
     ImGui::End();
 
-    // Show progress bar of loading additional dat files
-    // Draw a single progress bar for the cumulative progress
-    if (is_analyzing) {
-        draw_dat_load_progress_bar(total_additional_files_read, total_additional_files);
+    // Create maps for each dat file mapping filehash(file_id) to each entrys murmurhash.
+    // We don't include files where the file_id is the same (i.e. many files have the file_id 0) which I don't know how to compare between multiples dats
+    if (!is_analyzing && dat_fileid_to_mm3hash.size() < dat_managers.size()) {
+        all_dats_file_ids.clear();
+        dat_fileid_to_mm3hash.clear();
+        for (int i = 0; i < dat_managers.size(); i++) {
+            std::set<uint32_t> seen_file_ids;
+            std::unordered_map<uint32_t, uint32_t> new_map;
+            const auto& mft = dat_managers[i]->get_MFT();
+            for (int j = 0; j < mft.size(); j++) {
+                const auto file_id = mft[j].Hash;
+                const auto seen_it = seen_file_ids.find(file_id);
+                if (seen_it != seen_file_ids.end()) {
+                    // remove the entry from the new_map if it is already there. Otherwise do nothing
+                    // Basically we just don't want any entries in the map that has a file_id that is shared.
+                    const auto new_map_it = new_map.find(file_id);
+                    if (new_map_it != new_map.end()) {
+                        new_map.erase(file_id);
+                        all_dats_file_ids.erase(file_id);
+                    }
+                }
+                else {
+                    new_map.emplace(file_id, mft[j].murmurhash3);
+                    seen_file_ids.emplace(file_id);
+                    all_dats_file_ids.emplace(file_id);
+                }
+            }
+
+            dat_fileid_to_mm3hash.emplace_back(new_map);
+        }
     }
 }
