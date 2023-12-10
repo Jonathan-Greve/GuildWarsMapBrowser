7d7f229b6c70740f116881577cdf9b819ccafa61
Jonathan Bjørn Greve <jonagrev@gmail.com>
2023-12-11 00:16:32 +0800

    Can now import a csv file with custom file infos like: name, wiki url, map ids and more and edit the file indisde the program. You can also add new rows to the data.

---
+++ SourceFiles/draw_file_info_editor_panel.cpp
@@ -0,0 +1,376 @@
+#include "pch.h"
+#include "draw_file_info_editor_panel.h"
+#include "rapidcsv.h"
+#include <commdlg.h> 
+#include <filesystem>
+#include <ranges>
+
+extern FileType selected_file_type;
+extern uint32_t selected_item_hash;
+uint32_t prev_selected_item_hash = -1;
+
+bool verify_temp_file(const std::string& temp_filepath,
+    const std::vector<std::vector<std::string>>& expected_data,
+    const std::string& updated_item_hash) {
+    std::ifstream temp_file(temp_filepath);
+    std::string line;
+    size_t rowIdx = 0;
+
+    while (std::getline(temp_file, line)) {
+        std::stringstream linestream(line);
+        std::string cell;
+        std::vector<std::string> row;
+
+        while (std::getline(linestream, cell, ',')) {
+            row.push_back(cell);
+        }
+
+        if (row.size() != expected_data[rowIdx].size()) {
+            return false; // Mismatch in number of columns
+        }
+
+        for (size_t colIdx = 0; colIdx < row.size(); ++colIdx) {
+            // Skip comparison for the updated row
+            if (expected_data[rowIdx][0] == updated_item_hash) {
+                continue;
+            }
+
+            if (row[colIdx] != expected_data[rowIdx][colIdx]) {
+                return false; // Mismatch in data
+            }
+        }
+
+        rowIdx++;
+        if (rowIdx >= expected_data.size()) {
+            break; // All rows checked
+        }
+    }
+
+    return rowIdx == expected_data.size(); // Ensure all rows were checked
+}
+
+void save_csv(const std::string& filepath,
+    const std::vector<std::vector<std::string>>& data,
+    const std::string& updated_item_hash) {
+    std::string temp_filepath = filepath + ".tmp";
+    std::string backup_filepath = filepath + ".bak";
+
+    // Create a backup of the original file
+    std::filesystem::copy_file(filepath, backup_filepath, std::filesystem::copy_options::overwrite_existing);
+
+    // Write to temporary file
+    std::ofstream temp_file(temp_filepath);
+    for (const auto& row : data) {
+        for (size_t i = 0; i < row.size(); ++i) {
+            temp_file << row[i];
+            if (i < row.size() - 1) temp_file << ",";
+        }
+        temp_file << "\n";
+    }
+    temp_file.close();
+
+    // Verify the integrity of the temporary file
+    if (!verify_temp_file(temp_filepath, data, updated_item_hash)) {
+        std::filesystem::remove(temp_filepath);
+        return;
+    }
+
+    // Replace the original file with the temporary file
+    std::filesystem::rename(temp_filepath, filepath);
+
+    // Remove the backup file after successful save
+    std::filesystem::remove(backup_filepath);
+}
+
+bool is_type_texture(FileType type) {
+    switch (type)
+    {
+    case ATEXDXT1:
+    case ATEXDXT2:
+    case ATEXDXT3:
+    case ATEXDXT4:
+    case ATEXDXT5:
+    case ATEXDXTN:
+    case ATEXDXTA:
+    case ATEXDXTL:
+    case ATTXDXT1:
+    case ATTXDXT3:
+    case ATTXDXT5:
+    case ATTXDXTN:
+    case ATTXDXTA:
+    case ATTXDXTL:
+    case DDS:
+        return true;
+    default:
+        break;
+    }
+
+    return false;
+}
+
+std::wstring open_file_dialog() {
+    OPENFILENAME ofn;
+    WCHAR szFile[260] = { 0 };
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = NULL; // Replace with your window handle
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = L"CSV Files (*.csv)\0*.csv\0All Files (*.*)\0*.*\0";
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    if (GetOpenFileName(&ofn) == TRUE) {
+        return ofn.lpstrFile;
+    }
+
+    return L"";
+}
+
+std::vector<std::vector<std::string>> load_csv(const std::string& filepath) {
+    rapidcsv::Document doc(filepath, rapidcsv::LabelParams(-1, -1));
+    std::vector<std::vector<std::string>> data;
+    size_t rowCount = doc.GetRowCount();
+    size_t colCount = doc.GetColumnCount();
+
+    for (size_t rowIdx = 0; rowIdx < rowCount; ++rowIdx) {
+        std::vector<std::string> row;
+        for (size_t colIdx = 0; colIdx < colCount; ++colIdx) {
+            row.push_back(doc.GetCell<std::string>(colIdx, rowIdx));
+        }
+        data.push_back(row);
+    }
+    return data;
+}
+
+std::wstring csv_filepath = L"";
+
+void draw_file_info_editor_panel() {
+    static std::set<ModelTypes> selected_model_types;
+    static std::vector<std::vector<std::string>> csv_data;
+
+    std::string selected_item_hash_hex = std::format("0x{:08x}", selected_item_hash);
+
+    ImGui::Begin("Custom File Info");
+
+    // Load csv file if not already loaded
+    if (csv_data.empty()) {
+        if (ImGui::Button("Open CSV File")) {
+            csv_filepath = open_file_dialog();
+            if (!csv_filepath.empty()) {
+                std::string filepath(csv_filepath.begin(), csv_filepath.end());
+                csv_data = load_csv(filepath);
+            }
+        }
+    }
+    if (selected_item_hash == -1) {
+    }
+    else {
+        static int found_row_index = -1;
+        static bool edit_mode = false;
+
+        bool selected_item_hash_changed = prev_selected_item_hash != selected_item_hash;
+        if (selected_item_hash_changed) {
+            selected_model_types.clear();
+
+            edit_mode = false;
+            found_row_index = -1;
+            for (int i = 1; i < csv_data.size(); i++) {
+                const auto& row = csv_data[i];
+
+                uint32_t row_hash_int = -1; // max value (no such file_id in the dat, use as non-existance)
+
+                // check if the file_id in the csv start with 0x or 0X
+                if (row[0][0] == '0' && std::tolower(row[0][1]) == 'x') {
+                    row_hash_int = std::stoi(row[0], 0, 16);
+                }
+                else {
+                    row_hash_int = std::stoi(row[0]);
+                }
+
+                if (row_hash_int == selected_item_hash) {
+                    found_row_index = i;
+                    break;
+                }
+            }
+        }
+
+        // If no matching row is found, create a new one but we don't add it to the csv until the user press 'save'
+        static std::vector<std::string> row_backup;
+        std::vector<std::string> row(9);
+        if (found_row_index == -1 && selected_item_hash != -1) {
+            row[0] = selected_item_hash_hex;
+            row[8] = std::to_string(static_cast<int>(selected_file_type));
+            row_backup = row;
+        }
+
+        static std::string name_buf;
+        static std::string gwwiki_buf;
+        static std::string map_id_buf;
+        static bool is_explorable = false;
+        static bool is_outpost = false;
+        static bool is_pvp = false;
+        static std::string model_type;
+
+        if (found_row_index >= 0 && found_row_index < csv_data.size() && !edit_mode) {
+            row = csv_data[found_row_index];
+            name_buf = row[1];
+            gwwiki_buf = row[2];
+            map_id_buf = row[3];
+            is_explorable = row[4] == "yes";
+            is_outpost = row[5] == "yes";
+            is_pvp = row[6] == "yes";
+            model_type = row[7];
+
+            // Clear the current selection set
+            selected_model_types.clear();
+
+            // Split the model_type string by semicolons and convert each part to the enum value
+            for (auto token : std::views::split(model_type, ';')) {
+                std::string_view token_sv(&*token.begin(), std::ranges::distance(token));
+                ModelTypes type = StringToModelType(std::string(token_sv)); // Convert token to string and then to ModelTypes enum
+                if (type != ModelTypes::unknown) { // Check for a valid type
+                    selected_model_types.insert(type);
+                }
+            }
+        }
+
+        // Display the row's data
+        ImGui::Text("File ID: %s", row[0].c_str());
+
+
+        if (edit_mode) {
+            ImGui::InputText("Name", &name_buf);
+            ImGui::InputText("URL", &gwwiki_buf);
+
+            if (selected_file_type == FFNA_Type2) {
+                static ModelTypes current_selection = ModelTypes::unknown;
+
+                // Dropdown for selecting model types
+                if (ImGui::BeginCombo("##ModelTypes", "Select Model Type")) {
+                    // Loop through all model types
+                    for (int i = 0; i <= static_cast<int>(ModelTypes::unknown); ++i) {
+                        ModelTypes type = static_cast<ModelTypes>(i);
+                        if (!selected_model_types.contains(type)) {
+                            if (ImGui::Selectable(ModelTypeToString(type).c_str(), current_selection == type)) {
+                                if (type != ModelTypes::unknown) {
+                                    selected_model_types.insert(type);
+                                }
+                                current_selection = ModelTypes::unknown;
+                            }
+                        }
+                    }
+                    ImGui::EndCombo();
+                }
+
+                // Display selected model types and allow for their removal
+                for (auto it = selected_model_types.begin(); it != selected_model_types.end();) {
+                    std::string label = ModelTypeToString(*it) + "##Selected";
+                    if (ImGui::Button(("- " + label).c_str())) {
+                        it = selected_model_types.erase(it);
+                    }
+                    else {
+                        ++it;
+                    }
+                }
+
+                // Convert selected model types to a semicolon-separated string
+                model_type.clear();
+                for (const auto& type : selected_model_types) {
+                    if (!model_type.empty()) model_type += ";";
+                    model_type += ModelTypeToString(type);
+                }
+            }
+
+            if (ImGui::Button("Save")) {
+                edit_mode = false;
+                if (found_row_index == -1) {
+                    csv_data.push_back(row);
+                    found_row_index = csv_data.size() - 1;
+                }
+
+                // column 0 (file_id) and column 8 (file type) already set automatically
+
+                csv_data[found_row_index][1] = name_buf;
+                csv_data[found_row_index][2] = gwwiki_buf;
+
+                if (selected_file_type == FFNA_Type2) {
+                    csv_data[found_row_index][7] = model_type;
+                }
+                else if (selected_file_type == FFNA_Type3) {
+                    csv_data[found_row_index][3] = map_id_buf;
+                    csv_data[found_row_index][4] = is_explorable ? "yes" : "no";
+                    csv_data[found_row_index][5] = is_outpost ? "yes" : "no";
+                    csv_data[found_row_index][6] = is_pvp ? "yes" : "no";
+                }
+
+                std::string filepath(csv_filepath.begin(), csv_filepath.end());
+                save_csv(filepath, csv_data, selected_item_hash_hex);
+            }
+
+            ImGui::SameLine();
+            if (ImGui::Button("Cancel")) {
+                edit_mode = false;
+                row = row_backup;
+            }
+        }
+        else {
+            if (row[1] != "") {
+                ImGui::Text("Name: \"%s\"", row[1].c_str());
+            }
+            else {
+                ImGui::Text("Name: N/A");
+            }
+
+            if (row[2] != "") {
+                ImGui::Text("GW Wiki URL: \"%s\"", row[2].c_str());
+            }
+            else {
+                ImGui::Text("GW Wiki URL: N/A");
+            }
+
+            if (selected_file_type == FFNA_Type2) {
+                row[7].empty() ? ImGui::Text("Model Type: N/A") : ImGui::Text("Model Type: %s", row[7].c_str());
+            }
+
+            else if (selected_file_type == FFNA_Type3) {
+                const auto& map_ids_string = row[3];
+                ImGui::Text("Map ids: ");
+
+                std::stringstream ss(map_ids_string);
+                std::string map_id;
+                while (std::getline(ss, map_id, ';')) {
+                    if (!map_id.empty()) {
+                        ImGui::SameLine();
+                        ImGui::Text("%s, ", map_id.c_str());
+                    }
+                }
+
+                bool is_explorable = row[4] == "yes";
+                bool is_outpost = row[5] == "yes";
+                bool is_pvp = row[6] == "yes";
+
+                ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+                ImGui::Checkbox("Is explorable", &is_explorable);
+                ImGui::Checkbox("Is outpost", &is_outpost);
+                ImGui::Checkbox("Is PvP", &is_pvp);
+                ImGui::PopItemFlag();
+            }
+        }
+
+        if (!edit_mode && ImGui::Button("Edit")) {
+            edit_mode = true;
+
+            if (found_row_index != -1) {
+                row_backup = csv_data[found_row_index];
+            }
+        }
+
+        prev_selected_item_hash = selected_item_hash;
+    }
+
+    ImGui::End();
+}
---
+++ SourceFiles/draw_file_info_editor_panel.h
@@ -0,0 +1,140 @@
+#pragma once
+
+void draw_file_info_editor_panel();
+
+enum class ModelTypes
+{
+    // NPCs
+    npc_friendly, // For NPCs that are always friendly (to the best of your knowledge)
+    npc_enemy,// For NPCs that are always enemies (to the best of your knowledge)
+
+    // Props
+    prop_building, // Structures like houses, shops, wells, ruins, barns, and temples.
+    prop_foliage, // Trees, bushes, flowers, grass, vines, and other plant life.
+    prop_furniture, // Chairs, tables, bookcases, beds, benches and other decorations.
+    prop_nature, // Rocks, boulders, cliffs, water bodies, and other natural formations
+    prop_barrier, // Gates, walls, hedges, and other objects used to block or define boundaries.
+    prop_miscellaneous, // Items that don't fit into other prop categories
+
+    // Weapons
+    item_weapon_axe,
+    item_weapon_sword,
+    item_weapon_daggers,
+    item_weapon_hammer,
+    item_weapon_scythe,
+    item_weapon_spear,
+    item_weapon_bow,
+    item_weapon_wand,
+    item_weapon_staff,
+    item_weapon_focus,
+    item_weapon_shield,
+
+    // Armor
+    item_armor_helm,
+    item_armor_chest,
+    item_armor_hands,
+    item_armor_legs,
+    item_armor_feet,
+
+
+    // other item types
+    item_consumable, // alcohol, sweets
+    item_upgrade_component, // runes, insignias
+    item_trophy, // the items accepted by collectors
+    item_crafting, // Used for crafting armor and weapons: iron ingots, ectos etc.
+    item_key, // keys for openings chests, e.g. Lockpick
+    item_miniature, // Miniature like those from birthday gifts
+    item_polymock, // Polymock pieces
+    item_quest, // Items used during quests
+    item_storybook, // Hero's Handbook etc. 
+    item_miscellaneous, // When you don't know or it doesn't fit in the above.
+
+    // Other
+    miscellaneous, // When it doesn't fit an of the categories above or you don't know.
+
+    unknown // Used only internally
+};
+
+inline std::string ModelTypeToString(ModelTypes type)
+{
+    switch (type)
+    {
+    case ModelTypes::npc_friendly: return "npc_friendly";
+    case ModelTypes::npc_enemy: return "npc_enemy";
+    case ModelTypes::prop_building: return "prop_building";
+    case ModelTypes::prop_foliage: return "prop_foliage";
+    case ModelTypes::prop_furniture: return "prop_furniture";
+    case ModelTypes::prop_nature: return "prop_nature";
+    case ModelTypes::prop_barrier: return "prop_barrier";
+    case ModelTypes::prop_miscellaneous: return "prop_miscellaneous";
+    case ModelTypes::item_weapon_axe: return "item_weapon_axe";
+    case ModelTypes::item_weapon_sword: return "item_weapon_sword";
+    case ModelTypes::item_weapon_daggers: return "item_weapon_daggers";
+    case ModelTypes::item_weapon_hammer: return "item_weapon_hammer";
+    case ModelTypes::item_weapon_scythe: return "item_weapon_scythe";
+    case ModelTypes::item_weapon_spear: return "item_weapon_spear";
+    case ModelTypes::item_weapon_bow: return "item_weapon_bow";
+    case ModelTypes::item_weapon_wand: return "item_weapon_wand";
+    case ModelTypes::item_weapon_staff: return "item_weapon_staff";
+    case ModelTypes::item_weapon_focus: return "item_weapon_focus";
+    case ModelTypes::item_weapon_shield: return "item_weapon_shield";
+    case ModelTypes::item_armor_helm: return "item_armor_helm";
+    case ModelTypes::item_armor_chest: return "item_armor_chest";
+    case ModelTypes::item_armor_hands: return "item_armor_hands";
+    case ModelTypes::item_armor_legs: return "item_armor_legs";
+    case ModelTypes::item_armor_feet: return "item_armor_feet";
+    case ModelTypes::item_consumable: return "item_consumable";
+    case ModelTypes::item_upgrade_component: return "item_upgrade_component";
+    case ModelTypes::item_trophy: return "item_trophy";
+    case ModelTypes::item_crafting: return "item_crafting";
+    case ModelTypes::item_key: return "item_key";
+    case ModelTypes::item_miniature: return "item_miniature";
+    case ModelTypes::item_polymock: return "item_polymock";
+    case ModelTypes::item_quest: return "item_quest";
+    case ModelTypes::item_storybook: return "item_storybook";
+    case ModelTypes::item_miscellaneous: return "item_miscellaneous";
+    case ModelTypes::miscellaneous: return "miscellaneous";
+    default: return "unknown";
+    }
+}
+
+inline ModelTypes StringToModelType(const std::string& typeString)
+{
+    if (typeString == "npc_friendly") return ModelTypes::npc_friendly;
+    if (typeString == "npc_enemy") return ModelTypes::npc_enemy;
+    if (typeString == "prop_building") return ModelTypes::prop_building;
+    if (typeString == "prop_foliage") return ModelTypes::prop_foliage;
+    if (typeString == "prop_furniture") return ModelTypes::prop_furniture;
+    if (typeString == "prop_nature") return ModelTypes::prop_nature;
+    if (typeString == "prop_barrier") return ModelTypes::prop_barrier;
+    if (typeString == "prop_miscellaneous") return ModelTypes::prop_miscellaneous;
+    if (typeString == "item_weapon_axe") return ModelTypes::item_weapon_axe;
+    if (typeString == "item_weapon_sword") return ModelTypes::item_weapon_sword;
+    if (typeString == "item_weapon_daggers") return ModelTypes::item_weapon_daggers;
+    if (typeString == "item_weapon_hammer") return ModelTypes::item_weapon_hammer;
+    if (typeString == "item_weapon_scythe") return ModelTypes::item_weapon_scythe;
+    if (typeString == "item_weapon_spear") return ModelTypes::item_weapon_spear;
+    if (typeString == "item_weapon_bow") return ModelTypes::item_weapon_bow;
+    if (typeString == "item_weapon_wand") return ModelTypes::item_weapon_wand;
+    if (typeString == "item_weapon_staff") return ModelTypes::item_weapon_staff;
+    if (typeString == "item_weapon_focus") return ModelTypes::item_weapon_focus;
+    if (typeString == "item_weapon_shield") return ModelTypes::item_weapon_shield;
+    if (typeString == "item_armor_helm") return ModelTypes::item_armor_helm;
+    if (typeString == "item_armor_chest") return ModelTypes::item_armor_chest;
+    if (typeString == "item_armor_hands") return ModelTypes::item_armor_hands;
+    if (typeString == "item_armor_legs") return ModelTypes::item_armor_legs;
+    if (typeString == "item_armor_feet") return ModelTypes::item_armor_feet;
+    if (typeString == "item_consumable") return ModelTypes::item_consumable;
+    if (typeString == "item_upgrade_component") return ModelTypes::item_upgrade_component;
+    if (typeString == "item_trophy") return ModelTypes::item_trophy;
+    if (typeString == "item_crafting") return ModelTypes::item_crafting;
+    if (typeString == "item_key") return ModelTypes::item_key;
+    if (typeString == "item_miniature") return ModelTypes::item_miniature;
+    if (typeString == "item_polymock") return ModelTypes::item_polymock;
+    if (typeString == "item_quest") return ModelTypes::item_quest;
+    if (typeString == "item_storybook") return ModelTypes::item_storybook;
+    if (typeString == "item_miscellaneous") return ModelTypes::item_miscellaneous;
+    if (typeString == "miscellaneous") return ModelTypes::miscellaneous;
+    // If no match is found, return unknown
+    return ModelTypes::unknown;
+}
