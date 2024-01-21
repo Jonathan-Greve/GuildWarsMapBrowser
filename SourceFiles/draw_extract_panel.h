dcf0f266a86344291d656eb882fd2dfe2aed8179
Jonathan Bjørn Greve <jonagrev@gmail.com>
2024-01-22 00:57:10 +0800

    Error handling and bug fix

--- SourceFiles/draw_extract_panel.h
+++ SourceFiles/draw_extract_panel.h
@@ -20,7 +20,7 @@ struct ExtractPanelInfo {
     bool pixels_per_tile_changed = false;
     std::wstring save_directory = L"";
     ExtractPanel::ExtractPanelMapFileType map_render_extract_file_type = ExtractPanel::DDS;
-    ExtractPanel::ExtractMapType map_render_extract_map_type = ExtractPanel::AllMapsTopDownOrthographic;
+    ExtractPanel::ExtractMapType map_render_extract_map_type = ExtractPanel::CurrentMapNoViewChange;
 };
 
 void draw_extract_panel(ExtractPanelInfo& extract_panel_info, DATManager* dat_manager);
