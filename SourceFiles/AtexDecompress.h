aaf7cb68c28d14b124eabf22163566bfe02cc059
DubbleClick <m@pyc.ac>
2024-09-04 17:19:39 +0200

    rename functions back to AtexSubCodeN_Cpp to make switching between them easier

--- SourceFiles/AtexDecompress.h
+++ SourceFiles/AtexDecompress.h
@@ -44,4 +44,4 @@ constexpr unsigned char byte_79053D[] = {
 
 int DecompressAtex(int a, int b, int imageformat, int d, int e, int f, int g);
 void AtexDecompress(unsigned int* input, unsigned int unknown, unsigned int imageformat,
-                    SImageDescriptor ImageDescriptor, unsigned int* output);
+                    const SImageDescriptor& ImageDescriptor, unsigned int* output);
