d3b9022079fbe70e58c208ba87c147bb42554e5b
Jonathan-Greve <jonagrev@gmail.com>
2025-07-05 12:16:58 +0200

    Implemented Atex5 and 6 decompresssor in cpp rather than assembly

--- SourceFiles/AtexAsm.h
+++ SourceFiles/AtexAsm.h
@@ -6,7 +6,7 @@ void AtexSubCode1_Cpp(uint32_t* array1, uint32_t* array2, unsigned int count);
 void AtexSubCode2_Cpp(uint32_t* outBuffer, uint32_t* dcmpBuffer1, uint32_t* dcmpBuffer2, SImageData* imageData, unsigned int blockCount, unsigned int blockSize);
 void AtexSubCode3_Cpp(uint32_t* outBuffer, uint32_t* dcmpBuffer1, uint32_t* dcmpBuffer2, SImageData* imageData, unsigned int blockCount, unsigned int blockSize);
 void AtexSubCode4_Cpp(uint32_t* outBuffer, uint32_t* dcmpBuffer1, uint32_t* dcmpBuffer2, SImageData* imageData, unsigned int blockCount, unsigned int blockSize);
+void AtexSubCode5_Cpp(uint32_t* outBuffer, uint32_t* dcmpBuffer1, uint32_t* dcmpBuffer2, SImageData* imageData, unsigned int blockCount, unsigned int blockSize, unsigned int flag);
+uint32_t AtexSubCode6_Cpp(uint32_t* output, uint32_t colorValue, uint32_t flag);
 
-// Assembly implementations (still needed for 64-bit conversion)
-void AtexSubCode5_Asm(unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned int e, unsigned int f, unsigned int g);
 void AtexSubCode7_Asm(unsigned int a, unsigned int b);
