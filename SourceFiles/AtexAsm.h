d5f65825e71044831b94e97be39f31971ebf24f2
Jonathan-Greve <jonagrev@gmail.com>
2025-07-05 14:09:05 +0200

    Implemented atex 7 in cpp rather than asm. We have now gotten rid of all asm code.

--- SourceFiles/AtexAsm.h
+++ SourceFiles/AtexAsm.h
@@ -8,5 +8,4 @@ void AtexSubCode3_Cpp(uint32_t* outBuffer, uint32_t* dcmpBuffer1, uint32_t* dcmp
 void AtexSubCode4_Cpp(uint32_t* outBuffer, uint32_t* dcmpBuffer1, uint32_t* dcmpBuffer2, SImageData* imageData, unsigned int blockCount, unsigned int blockSize);
 void AtexSubCode5_Cpp(uint32_t* outBuffer, uint32_t* dcmpBuffer1, uint32_t* dcmpBuffer2, SImageData* imageData, unsigned int blockCount, unsigned int blockSize, unsigned int flag);
 uint32_t AtexSubCode6_Cpp(uint32_t* output, uint32_t colorValue, uint32_t flag);
-
-void AtexSubCode7_Asm(unsigned int a, unsigned int b);
+void AtexSubCode7_Cpp(uint32_t* outBuffer, unsigned int blockCount);
